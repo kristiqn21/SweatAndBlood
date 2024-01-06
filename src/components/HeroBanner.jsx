376fbdc35f1a7211ec4a9d4347a690309bb1fb3e
kristiqn21 <kiko200516@gmail.com>
2024-01-06 20:16:06 +0200

    Added navbar,herosection

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import Herobanner from '../assets/assets/images/banner.jpg'
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}>
+      <Typography color="#6B7280" fontWeight="600" fontSize="24px" mb={2}>
+        Best Exercises
+        <p>for</p>
+        <p>Muscle Growth</p>
+      </Typography>
+      <Typography fontWeight="700" sx={{fontSize: {lg: '44px', xs: '40px'}}} mb={3}>
+        Don't be afraid to
+        <p>Bleed</p>
+        <p>And Sweat</p>
+      </Typography>
+
+      <Button variant="contained" color="info" href="#exercises">Explore Exercises</Button>
+      <img src={Herobanner} alt="banner" className="hero-banner-img" style={{ marginTop: '50px' }}/>
+    </Box>
+  );
+};
+
+export default HeroBanner;
