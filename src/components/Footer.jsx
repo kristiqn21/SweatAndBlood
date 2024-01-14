d16749a8338e7cddf721a4c03e7d125c515387e7
kristiqn21 <kiko200516@gmail.com>
2024-01-14 16:31:14 +0200

    added a Footer with instagram and github icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Logo from '../assets/assets/images/sweatandblood-high-resolution-logo-black-transparent.png'
+import { FaInstagram, FaGithub } from 'react-icons/fa';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <Box mt='80px'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' width='300px' />
+        <Typography variant='h4' pb='40px' mt='10px'>
+            <a href='https://www.instagram.com/k_cankov21/' target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+            <a href='https://github.com/kristiqn21' target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
 
-export default Footer
+export default Footer;
